1ad2b1b64fd7b93ae3273fd49aa4d953bc3d874c
tqthy <22521461@gm.uit.edu.vn>
2024-03-13 17:32:29 +0700

    configuring JWT Strategy and implement GetUser decorator

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UsersRepository } from "./users.repository";
+import { JwtPayload } from "./jwt-payload.interface";
+import { User } from "./user.entity";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private usersRepository: UsersRepository) {
+        super({
+            secretOrKey: 'area51',
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        });
+    }
+
+    async validate(payload: JwtPayload): Promise<User> {
+        const { username } = payload;
+        const user: User = await this.usersRepository.findOneBy({ username });
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+
+        return user;
+    }
+}
