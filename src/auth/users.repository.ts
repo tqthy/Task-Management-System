ed497cd6ad453e8f0173cecca1e478d7bb0b94c1
tqthy <22521461@gm.uit.edu.vn>
2024-03-13 16:19:02 +0700

    feat: Sign Up

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,16 +1,41 @@
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { User } from "./user.entity";
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 export class UsersRepository extends Repository<User> {
-    constructor(
-        @InjectRepository(User)
-        private usersRepository: Repository<User>
-    ) {
-        super(
-            usersRepository.target,
-            usersRepository.manager,
-            usersRepository.queryRunner,
-          );
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {
+    super(
+      usersRepository.target,
+      usersRepository.manager,
+      usersRepository.queryRunner,
+    );
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === "23505") {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
     }
+  }
 }
